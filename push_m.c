d5981ecceb8f9f4f152858d3850677fbd1119178
Joseph Andrew <andrewjose711@gmail.com>
2022-06-17 00:23:58 +0100

    Pushes element to the stack

--- push_m.c
+++ push_m.c
@@ -13,9 +13,9 @@ void push_m(stack_t **stack, unsigned int line_number)
 
 	n_node = malloc(sizeof(stack_t));
 	if (n_node == NULL)
-		handle_error(4);
+		handle_err(4);
 	if (isdigit(info.arg[1]) > 0)
-		handle_error(5);
+		handle_err(5);
 	n_node->n = atoi(info.arg[1]);
 	if (info.type == STACK)
 	{
