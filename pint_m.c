98d43203097f1a64b28403ed7cb15378cd2327a8
Drewjoe <andrewjose711@gmail.com>
2022-06-18 17:46:12 +0100

    Prints the value at the top of the stack.

--- pint_m.c
+++ pint_m.c
@@ -3,7 +3,7 @@ i#include "monty.h"
 /**
  * pint_m - print value on top of `stack', or exit if stack is empty
  * @stack: double pointer to head of stack
- * @line_number: line number of current operation
+ * @line_number: line number of current op
  * Return: void
  */
 void pint_m(stack_t **stack, unsigned int line_number)
