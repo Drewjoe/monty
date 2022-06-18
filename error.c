1bbde0ecbea0fc760d84ac57d7ace8607b3eb151
Drewjoe <andrewjose711@gmail.com>
2022-06-18 13:50:52 -0700

    Error handler.

--- error.c
+++ error.c
@@ -62,3 +62,101 @@ void unknown_command(void)
 		free_info();
 }
 
+/**
+ * malloc_fail - error in case malloc fail
+ */
+void malloc_fail(void)
+{
+	fprintf(stderr, "Error: malloc failed\n");
+	free_info();
+}
+
+/**
+ * push_use - Usage of push command error
+ */
+void push_use(void)
+{
+	fprintf(stderr, "L%d: usage: push integer\n", info.l_number);
+	free_info();
+}
+
+/**
+ * pint_err - error when using pint command in empty stack
+ */
+void pint_err(void)
+{
+	fprintf(stderr, "L%d: can't pint, stack empty\n", info.l_number);
+	free_info();
+}
+
+/**
+ * pop_err - error when stack is empty
+ */
+void pop_err(void)
+{
+	fprintf(stderr, "L%d: can't pop an empty stack\n", info.l_number);
+	free_info();
+}
+
+/**
+ * swap_err - error when the stack len less than 2
+ */
+void swap_err(void)
+{
+	fprintf(stderr, "L%d: can't swap, stack too short\n", info.l_number);
+	free_info();
+}
+
+/**
+ * add_err - error when add command and stack too short
+ */
+void add_err(void)
+{
+	fprintf(stderr, "L%d: can't add, stack too short\n", info.l_number);
+	free_info();
+}
+
+/**
+ * sub_err - error when sub command and stack too short
+ */
+void sub_err(void)
+{
+	fprintf(stderr, "L%d: can't sub, stack too short\n", info.l_number);
+	free_info();
+}
+
+/**
+ * div_err - error when stack too short command div
+ */
+void div_err(void)
+{
+	fprintf(stderr, "L%d: can't div, stack too short\n", info.l_number);
+	free_info();
+}
+
+/**
+ * _zero - error when top stack is 0 can t do div or mod command
+ */
+void _zero(void)
+{
+	fprintf(stderr, "L%d: division by zero\n", info.l_number);
+	free_info();
+}
+
+/**
+ * mul_err - error when stack is too short to do mul command
+ */
+void mul_err(void)
+{
+	fprintf(stderr, "L%d: can't mul, stack too short\n", info.l_number);
+	free_info();
+}
+
+/**
+ * mod_err - error when stack is too short to do mod command
+ */
+void mod_err(void)
+{
+	frintf(stderr, "L%d: can't mod, stack too short\n", info.l_number);
+	free_info();
+}
