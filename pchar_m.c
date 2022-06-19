ffb04aa989ae77445e9767fefccc534bde867304
Joseph Andrew <andrewjose711@gmail.com>
2022-06-20 00:11:43 +0100

    Prints char at the top of the stack.

--- pchar_m.c
+++ pchar_m.c
@@ -10,14 +10,14 @@ void pchar_m(stack_t **stack, unsigned int line_number)
 {
 	if (!stack || !*stack)
 	{
-		dprintf(2, "L%d: can't pchar, stack empty\n", line_number);
-		free_stuff();
+		vfprintf(2, "L%d: can't pchar, stack empty\n", line_number);
+		free_stack();
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
 		dprintf(2, "L%d: can't pchar, value out of range\n", line_number);
-		free_stuff();
+		free_stack();
 		exit(EXIT_FAILURE);
 	}
 	printf("%c\n", (*stack)->n);
