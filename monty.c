ca5e64680c98cf6fa103ef4efaded8ed469378d4
Joseph Andrew <andrewjose711@gmail.com>
2022-06-19 12:11:45 +0100

    Monty main function

--- monty.c
+++ monty.c
@@ -1,46 +1,19 @@
 #include "monty.h"
-var_t var;
-info_t info;
 
 /**
- * main - Monty bytecode interpreter
- * @argc: number of arguments passed
- * @argv: array of argument strings
- * Return: EXIT_SUCCESS on success or EXIT_FAILURE on failure
+ * main - an interpreter for monty byteCodes files.
+ * @argc: Argument Count
+ * @argv: Argument Value
+ * Return: Exit_Succes(0) or Exit_Failure(1)
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	stack_t *stack = NULL;
-
-	info.l_number = 0;
-	info.fp = NULL;
-	char *arg = NULL,
-	info.cmd = NULL;
-	size_t n = 0;
-
-	var.queue = 0;
-	var.stack_len = 0;
-
+	info.type = STACK;
 	if (argc != 2)
+		handle_error(1);
+	else
 	{
-		handle_err(1);
+	treat_monty(argv[1]);
 	}
-	info.fp = fopen(argv[1], "r");
-	if (info.fp == NULL)
-	{
-		handle_err(2);
-	}
-	_exit(free_info, &arg);
-	_exit(free_list, &stack);
-	_exit(free_info, info.fp);
-	while (getline(&arg, &n, info.fp) != -1)
-	{
-		info.l_number++;
-		info.cmd = strtok(arg, "\n\t\r ");
-		if (info.cmd != NULL && info.cmd[0] != '#')
-		{
-			get_op(info.cmd, &stack, info.l_number);
-		}
-	}
-	exit(EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
